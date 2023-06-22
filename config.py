140895fd6fbe4780fc48a1ddf72bbb8e2aaaca84
nmasse <nicolas.masse@gmail.com>
2023-06-22 17:27:46 -0400

    larger network

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 
 dataset_cfg = {
-    "data_path": "/home/masse/work/perceiver/data",
+    "data_path": "/home/masse/work/perceiver/data_scaled",
     "predict_classes": {"CERAD": 4, "BRAAK_AD": 7, "class": 8},
-    "batch_size": 32,
+    "batch_size": 16,
     "num_workers": 16,
     "n_mask": 316,
     "rank_order": True,
 }
 
 trainer_cfg = {
-    "accumulate_grad_batches": 2,
+    "accumulate_grad_batches": 4,
     "precision": "bf16",
     "grad_clip_value": 0.5,
 }
@@ -25,12 +25,12 @@ task_cfg = {
 }
 
 model_cfg = {
-    "seq_dim": 256,
-    "query_len": 64,
-    "query_dim": 256,
-    "n_layers": 10,
+    "seq_dim": 512,
+    "query_len": 9,
+    "query_dim": 512,
+    "n_layers": 8,
     "dim_feedforward": 1024,
     "n_heads": 2,
     "dropout": 0.0,
-    "rank_oder": dataset_cfg["rank_order"]
+    "rank_order": dataset_cfg["rank_order"]
 }
