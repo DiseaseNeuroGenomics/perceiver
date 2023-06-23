984bd808e738e94d01308406718a57176466fa9c
nmasse <nicolas.masse@gmail.com>
2023-06-23 09:21:17 -0400

    minor updates

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ trainer_cfg = {
 
 task_cfg = {
     # "classify": True,
-    "learning_rate": 5e-5,
+    "learning_rate": 4e-5,
     "weight_decay": 0.00001,
-    "warmup_steps": 5_000,
+    "warmup_steps": 8_000,
     "decay_steps": 100_000,
     "decay": 0.999985,
 
@@ -28,7 +28,7 @@ model_cfg = {
     "seq_dim": 512,
     "query_len": 9,
     "query_dim": 512,
-    "n_layers": 8,
+    "n_layers": 12,
     "dim_feedforward": 1024,
     "n_heads": 2,
     "dropout": 0.0,
