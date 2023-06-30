c575f602aa6e039f71de97655de40d2ddb79dcb7
nmasse <nicolas.masse@gmail.com>
2023-06-29 22:50:57 -0400

    clean up

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
+
 import torch
 import pytorch_lightning as pl
-from pytorch_lightning import Trainer, seed_everything
-from datasets import DataModule, DataModuleAnndata
-from networks import Exceiver, extract_state_dict
+from datasets import DataModule
+from networks import Exceiver
 from tasks import MSELoss
 from config import dataset_cfg, task_cfg, model_cfg, trainer_cfg
 import warnings
@@ -11,18 +11,15 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 print(f"GPU is available {torch.cuda.is_available()}")
 torch.set_float32_matmul_precision('medium')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 
 def main():
 
     # Set seed
-    seed_everything(2299)
+    pl.seed_everything(2299)
 
     # Set up data module
-    # dm = DataModule(**dataset_cfg)
-    dm = DataModuleAnndata(**dataset_cfg)
+    dm = DataModule(**dataset_cfg)
 
     dm.setup(None)
 
@@ -31,13 +28,11 @@ def main():
     model_cfg["class_dist"] = dm.train_dataset.class_dist
 
     model = Exceiver(**model_cfg)
-    model.to(device=device)
 
     task = MSELoss(
         network=model,
         task_cfg=task_cfg,
     )
-
 
     trainer = pl.Trainer(
         accelerator='gpu',
