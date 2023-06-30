eb79e700777d8c7e608ba58906e7a4042c02ea3b
nmasse <nicolas.masse@gmail.com>
2023-06-30 19:51:45 -0400

    updated class list

--- config.py
+++ config.py
@@ -1,19 +1,18 @@
-
 dataset_cfg = {
-    "train_data_path": "/ssd/mssm_data/train_data.dat",
-    "train_metadata_path": "/ssd/mssm_data/train_metadata.pkl",
-    "test_data_path": "/ssd/rush_data/train_data.dat",
-    "test_metadata_path": "/ssd/rush_data/train_metadata.pkl",
-    "predict_classes": ["BRAAK_AD", "CERAD", "class"],
-    "batch_size": 64,
-    "num_workers": 12,
+    "train_data_path": "/sc/arion/projects/psychAD/massen06/mssm_data/train_data.dat",
+    "train_metadata_path": "/sc/arion/projects/psychAD/massen06/mssm_data/train_metadata.pkl",
+    "test_data_path": "/sc/arion/projects/psychAD/massen06/rush_data/train_data.dat",
+    "test_metadata_path": "/sc/arion/projects/psychAD/massen06/rush_data/train_metadata.pkl",
+    "predict_classes": ["AD", "ApoE_gt", "BRAAK_AD", "CERAD", "class", "Dementia"],
+    "batch_size": 128,
+    "num_workers": 6,
     "n_mask": 100,
     "rank_order": True,
 }
 
 trainer_cfg = {
     "accumulate_grad_batches": 1,
-    "precision": "bf16",
+    "precision": "bf16-mixed",
     "grad_clip_value": 0.5,
     "n_devices": 1,
 }
@@ -32,7 +31,7 @@ model_cfg = {
     "seq_dim": 512,
     "query_len": 64,
     "query_dim": 512,
-    "n_layers": 10,
+    "n_layers": 12,
     "dim_feedforward": 1024,
     "n_heads": 2,
     "dropout": 0.0,
