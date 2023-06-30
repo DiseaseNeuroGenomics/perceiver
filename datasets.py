fix to masking; train/test from different souurces

datasets.py
@@ -21,8 +21,7 @@ class SingleCellDataset(Dataset):
         metadata_path: str,
         cells_per_epochs: int,
         predict_classes: Optional[List[str]] = None,
-        n_min_mask: int = 1,
-        n_max_mask: int = 500,
+        n_mask: int = 100,
         batch_size: int = 32,
         rank_order: bool = True,
         scale_by_max: bool = False,
@@ -36,8 +35,7 @@ class SingleCellDataset(Dataset):
         self.n_samples = len(self.metadata["obs"]["class"])
         self.n_genes = len(self.metadata["var"])
         self.n_classes = len(predict_classes) if predict_classes is not None else 0
-        self.n_min_mask = n_min_mask
-        self.n_max_mask = n_max_mask
+        self.n_mask = n_mask
         self.batch_size = batch_size
         self.rank_order = rank_order
         self.scale_by_max = scale_by_max
@@ -78,13 +76,13 @@ class SingleCellDataset(Dataset):
         else:
             self.class_unique = self.class_dist = None
 
-    def _get_class_vals(self, idx: List[int]):
+    def _get_class_vals(self, batch_idx: List[int]):
         """Extract the class value for ach entry in the batch"""
         if self.class_unique is None:
             return None
 
         class_vals = np.zeros((self.batch_size, self.n_classes), dtype=np.int64)
-        for n0, i in enumerate(idx):
+        for n0, i in enumerate(batch_idx):
             for n1, (k, v) in enumerate(self.class_unique.items()):
                 idx = np.where(self.metadata["obs"][k][i] == v)
                 # class values of -1 will imply N/A, and will be masked out
@@ -92,10 +90,10 @@ class SingleCellDataset(Dataset):
 
         return torch.from_numpy(class_vals)
 
-    def _get_gene_vals(self, idx: List[int]):
+    def _get_gene_vals(self, batch_idx: List[int]):
 
         gene_vals = np.zeros((self.batch_size, self.n_genes), dtype=np.float32)
-        for n, i in enumerate(idx):
+        for n, i in enumerate(batch_idx):
             gene_vals[n, :] = np.memmap(
                 self.data_path, dtype='float16', mode='r', shape=(self.n_genes,), offset=i * self.offset
             ).astype(np.float32)
@@ -110,12 +108,12 @@ class SingleCellDataset(Dataset):
     def _prepare_data(self, batch_idx):
 
         gene_vals, gene_targets, zero_idx = self._get_gene_vals(batch_idx)
-        class_vals = self._get_class_vals(batch_idx)
+        class_targets = self._get_class_vals(batch_idx)
 
         key_padding_mask = torch.zeros_like(gene_vals).detach()
         key_padding_mask[zero_idx[0], zero_idx[1]] = 1.0
 
-        return gene_vals, key_padding_mask, gene_targets, class_vals
+        return gene_vals, key_padding_mask, gene_targets, class_targets
 
     def __len__(self):
         return self.cells_per_epochs
@@ -128,18 +126,23 @@ class SingleCellDataset(Dataset):
         gene_vals, key_padding_mask, gene_targets, class_targets = self._prepare_data(idx)
 
         # mask indices
-        rnd_idx = []
+        mask_col_ids = []
+        mask_row_ids = []
         for i in range(self.batch_size):
             nonzero_idx = torch.where(key_padding_mask[i, :] == 0)[0]
             # randomly choose number to mask for each cell
-            n = np.random.randint((self.n_min_mask, self.n_max_mask))
-            k = np.random.choice(nonzero_idx, n, replace=False)
-            rnd_idx.append(k)
+            # n = np.random.randint(0, self.n_mask)
+            mask_idx = np.random.choice(nonzero_idx, self.n_mask, replace=False)
+            for j in mask_idx:
+                mask_row_ids.append(i)
+                mask_col_ids.append(j)
+        """
         rnd_idx = np.concatenate(rnd_idx, axis=-1)
         mask_col_ids = torch.tensor(rnd_idx).long()
         mask_row_ids = torch.repeat_interleave(
             torch.arange(0, self.batch_size), self.n_mask
         ).long()
+        """
 
         assert len(mask_col_ids) == len(mask_row_ids)
 
@@ -162,7 +165,6 @@ class SingleCellDataset(Dataset):
             gene_ids[i, zero_idx] = self.n_genes
             gene_vals[i, zero_idx] = 0.0 #  should already be zero...just to be safe
 
-
         batch = gene_ids, gene_target_ids, class_ids, gene_vals, gene_targets, key_padding_mask, class_targets
 
         if self.pin_memory:
@@ -179,7 +181,7 @@ class AnnDataset(SingleCellDataset):
         cell_idx: List[int],
         gene_idx: List[int],
         cells_per_epochs: int,
-        predict_classes: Optional[Dict[str, int]] = None,
+        predict_classes: Optional[List[str]] = None,
         n_mask: int = 316,
         batch_size: int = 32,
         rank_order: bool = True,
@@ -218,7 +220,7 @@ class AnnDataset(SingleCellDataset):
         if self.predict_classes is not None:
             self.class_unique = {}
             self.class_dist = {}
-            for k in self.predict_classes.keys():
+            for k in self.predict_classes:
                 unique_list, counts = np.unique(self.anndata.obs[k], return_counts=True)
                 self.class_unique[k] = np.array(unique_list)
                 self.class_dist[k] = counts / np.max(counts)
@@ -282,43 +284,50 @@ class DataModule(pl.LightningDataModule):
 
     def __init__(
         self,
-        data_path: str,
+        train_data_path: str,
+        train_metadata_path: str,
+        test_data_path: str,
+        test_metadata_path: str,
         batch_size: int = 32,
         num_workers: int = 16,
-        n_min_mask: int = 1,
-        n_max_mask: int = 100,
+        n_mask: int = 100,
         rank_order: bool = False,
         predict_classes: Optional[Dict[str, int]] = None,
+        n_train_cells: int = 256_000,
+        n_test_cells: int = 25_6000,
+
     ):
         super().__init__()
-        self.data_path = data_path
+        self.train_data_path = train_data_path
+        self.train_metadata_path = train_metadata_path
+        self.test_data_path = test_data_path
+        self.test_metadata_path = test_metadata_path
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.n_max_mask = n_max_mask
-        self.rank_order = rank_order
+        self.n_mask = n_mask
         self.rank_order = rank_order
         self.predict_classes = predict_classes
+        self.n_train_cells = n_train_cells
+        self.n_test_cells = n_test_cells
 
     def setup(self, stage):
 
         self.train_dataset = SingleCellDataset(
-            os.path.join(self.data_path, "train_data.dat"),
-            os.path.join(self.data_path, "train_metadata.pkl"),
-            32 * 2000,
+            self.train_data_path,
+            self.train_metadata_path,
+            self.n_train_cells,
             predict_classes=self.predict_classes,
-            n_min_mask=self.n_min_mask,
-            n_max_mask=self.n_max_mask,
+            n_mask=self.n_mask,
             batch_size=self.batch_size,
             rank_order=self.rank_order,
             pin_memory=False,
         )
         self.val_dataset = SingleCellDataset(
-            os.path.join(self.data_path, "test_data.dat"),
-            os.path.join(self.data_path, "test_metadata.pkl"),
-            32 * 200,
+            self.test_data_path,
+            self.test_metadata_path,
+            self.n_test_cells,
             predict_classes=self.predict_classes,
-            n_min_mask=self.n_min_mask,
-            n_max_mask=self.n_max_mask,
+            n_mask=self.n_mask,
             batch_size=self.batch_size,
             rank_order=self.rank_order,
             pin_memory=False,
@@ -345,7 +354,7 @@ class DataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         sampler = BatchSampler(
-            SequentialSampler(self.val_dataset),
+            RandomSampler(self.val_dataset),
             batch_size=self.val_dataset.batch_size,
             drop_last=True,
         )
@@ -376,7 +385,7 @@ class DataModuleAnndata(pl.LightningDataModule):
         n_min_mask: int = 1,
         n_max_mask: int = 100,
         rank_order: bool = False,
-        predict_classes: Optional[Dict[str, int]] = None,
+        predict_classes: Optional[List[str]] = None,
         gene_min_pct_threshold: float = 0.02,
         min_genes_per_cell: int = 1000,
         train_pct: float = 0.9,
@@ -403,7 +412,7 @@ class DataModuleAnndata(pl.LightningDataModule):
             self.anndata,
             self.train_idx,
             self.gene_idx,
-            32 * 2000,
+            128 * 2000,
             predict_classes=self.predict_classes,
             n_min_mask=self.n_min_mask,
             n_max_mask=self.n_max_mask,
@@ -415,7 +424,7 @@ class DataModuleAnndata(pl.LightningDataModule):
             self.anndata,
             self.test_idx,
             self.gene_idx,
-            32 * 200,
+            128 * 100,
             predict_classes=self.predict_classes,
             n_min_mask=self.n_min_mask,
             n_max_mask=self.n_max_mask,
@@ -471,7 +480,7 @@ class DataModuleAnndata(pl.LightningDataModule):
 
     def val_dataloader(self):
         sampler = BatchSampler(
-            SequentialSampler(self.val_dataset),
+            RandomSampler(self.val_dataset),
             batch_size=self.val_dataset.batch_size,
             drop_last=True,
         )