major fix to class masking

datasets.py
@@ -74,11 +74,12 @@ class SingleCellDataset(Dataset):
             for k in self.predict_classes:
                 unique_list, counts = np.unique(self.metadata["obs"][k], return_counts=True)
                 # remove nans, negative values, or anything else suspicious
-                idx = [n for n, u in enumerate(unique_list) if isinstance(u, str) or u >= 0 or u <= 999]
+                idx = [n for n, u in enumerate(unique_list) if isinstance(u, str) or (u >= 0 and u <= 999)]
                 unique_list = unique_list[idx]
                 counts = counts[idx]
                 self.class_unique[k] = np.array(unique_list)
                 self.class_dist[k] = counts / np.max(counts)
+                print("class info", k, self.class_unique[k], self.class_dist[k])
         else:
             self.class_unique = self.class_dist = None
 