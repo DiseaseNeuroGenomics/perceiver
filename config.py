c643e1e0dfa43b239bff0744d0f2e6ba86669926
nmasse <nicolas.masse@gmail.com>
2023-07-07 00:15:32 -0400

    minor updates

--- config.py
+++ config.py
@@ -15,51 +15,49 @@ cell_properties = {
 
 
 dataset_cfg = {
-    "train_data_path": "/ssd/mssm_raw_data/train_data.dat",
-    "train_metadata_path": "/ssd/mssm_raw_data/train_metadata.pkl",
-    "test_data_path": "/ssd/mssm_raw_data/test_data.dat",
-    "test_metadata_path": "/ssd/mssm_raw_data/test_metadata.pkl",
-    #"train_data_path": "/sc/arion/projects/psychAD/massen06/mssm_raw_data/train_data.dat",
-    #"train_metadata_path": "/sc/arion/projects/psychAD/massen06/mssm_raw_data/train_metadata.pkl",
-    #"test_data_path": "/sc/arion/projects/psychAD/massen06/rush_raw_data/train_data.dat",
-    #"test_metadata_path": "/sc/arion/projects/psychAD/massen06/rush_raw_data/train_metadata.pkl",
+    "train_data_path": "/ssd/mssm_rush_data/train_data.dat",
+    "train_metadata_path": "/ssd/mssm_rush_data/train_metadata.pkl",
+    "test_data_path": "/ssd/mssm_rush_data/test_data.dat",
+    "test_metadata_path": "/ssd/mssm_rush_data/test_metadata.pkl",
     #"train_data_path": "/home/masse/work/perceiver/rush_raw_data/train_data.dat",
     #"train_metadata_path": "/home/masse/work/perceiver/rush_raw_data/train_metadata.pkl",
     #"test_data_path": "/home/masse/work/perceiver/rush_raw_data/test_data.dat",
     #"test_metadata_path": "/home/masse/work/perceiver/rush_raw_data/test_metadata.pkl",
     "cell_properties": cell_properties,
-    "batch_size": 64,
-    "num_workers": 6,
+    "batch_size": 16,
+    "num_workers": 10,
     "n_mask": 200,
     "rank_order": False,
     "cell_prop_same_ids": False,
-    "cutmix_pct": 0.9,
+    "cutmix_pct": 0.0,
+    "mixup": True,
 }
 
 trainer_cfg = {
     "accumulate_grad_batches": 1,
     "precision": "bf16-mixed",
     "grad_clip_value": 0.5,
-    "n_devices": 2,
+    "n_devices": 1,
 }
 
 task_cfg = {
     # "classify": True,
-    "learning_rate": 0.00005,
+    "learning_rate": 1.2e-4,
     "weight_decay": 0.00001,
     "warmup_steps": 12_000,
     "decay_steps": 100_000,
     "decay": 0.999988,
     "balance_classes": True,
     "adverserial": False,
-
+    "gene_weight": 0.5,
+    "cell_prop_weight": 1.0,
 }
 
 model_cfg = {
     "seq_dim": 512,
-    "query_len": 64,
+    "query_len": 128,
     "query_dim": 512,
-    "n_layers": 12,
+    "n_layers": 16,
     "dim_feedforward": 1024,
     "n_heads": 2,
     "dropout": 0.0,
@@ -67,18 +65,19 @@ model_cfg = {
 }
 
 test_cfg = {
-    #"ckpt_path": "/home/masse/work/perceiver/mssm_model/epoch19_v52.ckpt",
-    "ckpt_path": "/home/masse/work/perceiver/lightning_logs/version_13/saved_model.ckpt"
+    "ckpt_path": "/home/masse/work/perceiver/mssm_model/epoch41_v116.ckpt",
+    #"ckpt_path": "/home/masse/work/perceiver/lightning_logs/version_13/saved_model.ckpt"
 }
 
 test_dataset_cfg = {
-    "train_data_path": "/home/masse/work/perceiver/rush_raw_data/test_data.dat",
-    "train_metadata_path": "/home/masse/work/perceiver/rush_raw_data/test_metadata.pkl",
-    "test_data_path": "/home/masse/work/perceiver/rush_raw_data/test_data.dat",
-    "test_metadata_path": "/home/masse/work/perceiver/rush_raw_data/test_metadata.pkl",
+    "train_data_path": "/home/masse/work/perceiver/mssm_rush_data/train_data.dat",
+    "train_metadata_path": "/home/masse/work/perceiver/mssm_rush_data/train_metadata.pkl",
+    "test_data_path": "/home/masse/work/perceiver/mssm_rush_data/test_data.dat",
+    "test_metadata_path": "/home/masse/work/perceiver/mssm_rush_data/test_metadata.pkl",
     "cell_properties": cell_properties,
     "batch_size": 16,
     "num_workers": 10,
-    "n_mask": 200,
+    "n_mask": 50,
     "rank_order": False,
+    "cell_prop_same_ids": False,
 }
