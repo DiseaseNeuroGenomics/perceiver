37d8cf6c8da190365775f4eb62de89bd97c19934
nmasse <nicolas.masse@gmail.com>
2023-07-11 11:28:48 -0400

    data now stored as uint8

--- create_dataset.py
+++ create_dataset.py
@@ -18,26 +18,19 @@ class CreateData:
         source_paths: List[str],
         target_path: str,
         train_pct: float = 0.9,
-        rank_order: bool = False,
-        normalize_total: Optional[float] = None,
-        log_normalize: bool = False,
         min_genes_per_cell: int = 1000,
         min_percent_cells_per_gene: float = 0.02,
         split_train_test_by_subject: bool = True,
+        protein_coding_only: bool = True,
     ):
         self.source_paths = source_paths
         self.target_path = target_path
         self.train_pct = train_pct
-        self.rank_order = rank_order
-        if rank_order:
-            print("Since rank_oder=True, setting normalize_total=None and log_normalize=False")
-            normalize_total = None
-            log_normalize = False
-        self.normalize_total = normalize_total
-        self.log_normalize = log_normalize
+
         self.min_genes_per_cell = min_genes_per_cell
         self.min_percent_cells_per_gene = min_percent_cells_per_gene
         self.split_train_test_by_subject = split_train_test_by_subject
+        self.protein_coding_only = protein_coding_only
 
         self.obs_keys = [
             'CERAD', 'BRAAK_AD', 'BRAAK_PD', 'Dementia', 'AD', 'class', 'subclass', 'subtype', 'ApoE_gt',
@@ -100,47 +93,6 @@ class CreateData:
         self.train_idx = np.sort(self.train_idx)
         self.test_idx = np.sort(self.test_idx)
 
-    def _calculate_stats(self, fp):
-
-        n_cells, n_features = fp.shape
-
-        count = 0
-        sum_all = np.zeros(n_features, dtype=np.float32)
-        sumsq = np.zeros(n_features, dtype=np.float32)
-        self.max_vals = np.zeros(n_features, dtype=np.float32)
-        count_nonzero = np.ones(n_features, dtype=np.float32)  # add one to stabilize stats
-        for i in range(n_cells):
-            y = np.array(fp[i, :]).astype(np.float32)
-            count += 1
-            sum_all += y
-            sumsq += y**2
-            idx = np.where(y > 0)[0]
-            count_nonzero[idx] += 1
-            idx_max = np.where(y > self.max_vals)[0]
-            self.max_vals[idx_max] = y[idx_max]
-
-        self.mean = sum_all / count
-        self.mean_nonzero = sum_all / count_nonzero
-        self.std = np.sqrt((sumsq / count) - (self.mean**2))
-        self.std_nonzero = np.sqrt((sumsq / count_nonzero) - (self.mean_nonzero ** 2))
-
-    def _normalize(self, x: np.ndarray) -> np.ndarray:
-        x = x * self.normalize_total / np.sum(x, axis=1, keepdims=True) if self.normalize_total is not None else x
-        x = np.log1p(x) if self.log_normalize else x
-        return x
-
-    def _rank_order(self, x: np.ndarray) -> np.ndarray:
-        """Will assign scores from 0 (lowest) to 1 (highest)."""
-        cell_rank = np.zeros_like(x)
-        for i in range(x.shape[0]):
-            unique_counts = np.unique(x[i, :])
-            rank_score = np.linspace(0.0, 1.0, len(unique_counts))
-            for n, count in enumerate(unique_counts):
-                idx = np.where(x[i, :] == count)[0]
-                cell_rank[i, idx] = rank_score[n]
-
-        return cell_rank
-
     def _create_metadata(self, train: bool = True):
 
         idx = self.train_idx if train else self.test_idx
@@ -150,16 +102,6 @@ class CreateData:
         meta = {
             "obs": {k: self.anndata.obs[k][idx].values for k in self.obs_keys},
             "var": {k: self.anndata.var[k][self.gene_idx].values for k in self.var_keys},
-            "stats": {
-                "mean": self.mean,
-                "std": self.std,
-                "max": self.max_vals,
-                "mean_nonzero": self.mean,
-                "std_nonzero": self.std,
-                "normalize_total": self.normalize_total,
-                "log_normalize": self.log_normalize,
-                "rank_order": self.rank_order,
-            },
         }
 
         pickle.dump(meta, open(meatadata_fn, "wb"))
@@ -167,7 +109,11 @@ class CreateData:
     def _get_gene_index(self):
 
         if "percent_cells" in self.anndata.var.keys():
-            self.gene_idx = np.where(self.anndata.var["percent_cells"] > 100 * self.min_percent_cells_per_gene)[0]
+            self.gene_idx = self.adata.var["percent_cells"] > self.min_percent_of_cells
+            if self.protein_coding_only:
+                self.gene_idx *= self.adata.var["protein_coding"]
+
+            self.gene_idx = np.where(self.gene_idx)[0]
         else:
             chunk_size = 10_000
             n_segments = 10
@@ -196,22 +142,21 @@ class CreateData:
         data_fn = "train_data.dat" if train else "test_data.dat"
         data_fn = os.path.join(self.target_path, data_fn)
         n_genes = len(self.gene_idx)
+        print(f"Creating data. Number of cell: {len(idx)}, number of genes: {n_genes}")
 
-        chunk_size = 25_000  # chunk size for loading data into memory
-        fp = np.memmap(data_fn, dtype='float16', mode='w+', shape=(len(idx), n_genes))
+        chunk_size = 10_000  # chunk size for loading data into memory
+        fp = np.memmap(data_fn, dtype='unint8', mode='w+', shape=(len(idx), n_genes))
 
         for n in range(int(np.ceil(len(idx) / chunk_size))):
             m = np.minimum(len(idx), (n + 1) * chunk_size)
             current_idx = idx[n * chunk_size: m]
             print(f"Creating dataset, cell number = {current_idx[0]}")
             y = self.anndata[current_idx]
-            y = y.X.toarray().astype(np.float32)
+            y = y.X.toarray()
             y = y[:, self.gene_idx]
-            if self.rank_order:
-                y = self._rank_order(y)
-            else:
-                y = self._normalize(y)
-            fp[n * chunk_size: m, :] = y.astype(np.float16)
+            y[y >= 255] = 255
+            y = y.astype(np.uint8)
+            fp[n * chunk_size: m, :] = y
 
         # flush to memory
         fp.flush()
@@ -248,7 +193,7 @@ if __name__ == "__main__":
         base_dir + "MSSM_2023-06-08_22_31.h5ad",
     ]
     target_path = "/sc/arion/projects/psychAD/massen06/mssm_rush_data"
-    c = CreateData(source_paths, target_path, train_pct=0.9, rank_order=False)
+    c = CreateData(source_paths, target_path, train_pct=0.9)
 
     c.create_datasets()
 
